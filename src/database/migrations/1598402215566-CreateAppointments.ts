b8690aae77c0ef66533c0fe6948b1b7c817eff14
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2020-08-25 23:44:44 -0300

    Relating models

--- src/database/migrations/1598402215566-CreateAppointments.ts
+++ src/database/migrations/1598402215566-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1598402215566
                 columns: [
                     {
                         name: 'id',
-                        type: 'varchar',
+                        type: 'uuid',
                         isPrimary: true,
                         generationStrategy: 'uuid',
                         default: 'uuid_generate_v4()',
