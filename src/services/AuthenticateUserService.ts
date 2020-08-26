c64d23b92ad2a91bcf12704293365737e5977a8b
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2020-08-26 02:22:23 -0300

    Authenticated routes

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -28,9 +29,11 @@ class AuthenticateUserService {
             throw new Error('Incorrect email/password combination.');
         }
 
-        const token = sign({}, '63373b41cf913e9f9b3226b4a0452737', {
+        const { secret, expiredIn } = authConfig.jwt;
+
+        const token = sign({}, secret, {
             subject: user.id,
-            expiresIn: '1d',
+            expiresIn: expiredIn,
         });
 
         return {
