8d4ac479b4c1bf6f518fe72c72972d3ffcc90a04
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2020-08-26 09:39:59 -0300

    Added feature upload avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUsersService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
@@ -21,8 +22,6 @@ usersRouter.post('/', async (request, response) => {
             password,
         });
 
-        delete user.password;
-
         return response.json(user);
     } catch (err) {
         return response.status(400).json({ error: err.message });
@@ -34,7 +33,20 @@ usersRouter.patch(
     ensureAuthenticated,
     upload.single('avatar'),
     async (request, response) => {
-        return response.json({ ok: true });
+        try {
+            const updateUserAvatar = new UpdateUserAvatarService();
+
+            const user = await updateUserAvatar.execute({
+                user_id: request.user.id,
+                avatarFileName: request.file.filename,
+            });
+
+            delete user.password;
+
+            return response.json(user);
+        } catch (err) {
+            return response.status(400).json({ error: err.message });
+        }
     },
 );
 
