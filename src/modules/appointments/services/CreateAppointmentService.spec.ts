4d28e367cbb1ce4e0bb897fc31c05c6077868e09
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2020-09-15 00:14:26 -0300

    Error

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,36 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+    it('should be able to create a new appointment', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        const createAppointment = new CreateAppointmentService(
+            fakeAppointmentsRepository,
+        );
+
+        const appointment = await createAppointment.execute({
+            date: new Date(),
+            provider_id: '1223213112312',
+        });
+
+        expect(appointment).toHaveProperty('id');
+        expect(appointment.provider_id).toBe('1223213112312');
+    });
+
+    it('should not be able to create two appointments on the same time', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        const createAppointment = new CreateAppointmentService(
+            fakeAppointmentsRepository,
+        );
+        const appointmentDate = new Date(2020, 4, 10, 11);
+
+        expect(
+            createAppointment.execute({
+                date: appointmentDate,
+                provider_id: '1223213112312',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+});
