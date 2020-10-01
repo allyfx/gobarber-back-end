a4f49743393e1e152a93e0c7bcd195255dc3fc0d
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2020-10-01 20:35:37 -0300

    Fix features

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -64,6 +64,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
                 `,
                 ),
             },
+            relations: ['user'],
         });
 
         return appointments;
